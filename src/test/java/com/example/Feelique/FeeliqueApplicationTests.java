fb78b59ee60db259a7919f79d8db4a63b264750e
yyyyasmin <yasmin.yelgin@student.htw-berlin.de>
2025-11-28 14:16:00 +0100

    Initial Commit

---
+++ src/test/java/com/example/Feelique/FeeliqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Feelique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeeliqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
